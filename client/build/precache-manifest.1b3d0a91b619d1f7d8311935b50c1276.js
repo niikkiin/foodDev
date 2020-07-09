5f4be6c6193c928eeedd23aeb9e3aa361ab11353
Nikki <abarcanikki@gmail.com>
2020-07-09 20:30:46 +0800

    prepare to test deploy on netlify

---
+++ client/build/precache-manifest.1b3d0a91b619d1f7d8311935b50c1276.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cdf049daa329d8be1aaf028523ba15f1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cd049dfa43ae1ba75037",
+    "url": "/static/js/2.a254ee3d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.a254ee3d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "183865ac6fa754654bfc",
+    "url": "/static/js/3.259bdb79.chunk.js"
+  },
+  {
+    "revision": "2f48e5251ed0c94f5a94",
+    "url": "/static/js/main.510d8836.chunk.js"
+  },
+  {
+    "revision": "4dd9585e27936963aac3",
+    "url": "/static/js/runtime-main.83354847.js"
+  },
+  {
+    "revision": "5ad58eee1803345ceef587a5039fc90b",
+    "url": "/static/media/signInSVG.5ad58eee.svg"
+  },
+  {
+    "revision": "e979e21f6543ce4128438bb7ca8f2f79",
+    "url": "/static/media/signUpSVG.e979e21f.svg"
+  }
+]);
